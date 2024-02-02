f6f8a3e8840d1cba2f868fa15faf853fc8c1682e
Dreamy Undertaker <vadiimreg73@gmail.com>
2024-02-03 01:56:33 +0400

    Registration and signin ready

--- animal/db/views.py
+++ animal/db/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+# from .models import Articles, Persons, Deposites, ExchangeRates, Clients, DepositsRegistration
+# from .forms import ArticleForm, ClientsForm, DepositesForm, DepositsRegistrationForm, ExchangeRatesForm, PersonsForm
+
+# Create your views here.
+def db_home(request):
+    # db = Articles.objects.all()
+    # db1 = Persons.objects.all()
+    # db2 = Deposites.objects.all()
+    # db3 = ExchangeRates.objects.all()
+    # db4 = Clients.objects.all()
+    # db5 = DepositsRegistration.objects.all()
+    return render(request, 'db/db.html', )
--- animal/home/urls.py
+++ animal/home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', views.registration, name = 'registration'),
+    path('singin', views.signin, name='signin'),
+    path('home', views.home, name='home'),
 ]
